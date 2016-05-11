88155af76c817adf9d9df5beeaaefdf6fb0e0bb3
pjy <731401082@qq.com>
2016-05-11 23:56:40 +0800

    version 2.0,edit by gulp + sass

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+  return sass('app/scss/**/*.scss')
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({ stream:true }));
+});
+
+gulp.task('serve', ['sass'], function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/*.html', reload);
+});
